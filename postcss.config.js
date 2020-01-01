b8fcbf13deb7c0d7d43904c4a173079703faef74
Jethro Kuan <jethrokuan95@gmail.com>
2020-01-01 17:07:21 +0800

    add back purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,13 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
     require('cssnano'),
-  ]
+    purgecss({
+      content: ['./public/**/*.html'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    })
+]
 }
