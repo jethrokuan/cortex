2b0b5abe3da1cc84c413422fa0856803e5d584f3
Jethro Kuan <jethrokuan95@gmail.com>
2020-01-02 16:25:04 +0800

    updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,16 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./public/**/*.html'],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
     require('cssnano'),
-    purgecss({
-      content: ['./public/**/*.html'],
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-    })
-]
+    ...process.env.NODE_ENV === 'production'
+    ? [purgecss]
+    : []
+  ]
 }
